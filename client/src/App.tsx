931de37d9bbd5456064eca1c150262a4ef2a4933
kubenkodeklubb <kubenkodeklubb@gmail.com>
2019-12-16 14:13:34 +0100

    ui: set up router

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,61 @@
-import React from 'react'
+import React, { PropsWithChildren } from 'react'
+import {
+  EuiPage,
+  EuiPageBody,
+  EuiEmptyPrompt,
+  EuiTabs,
+  EuiTab,
+  EuiSpacer,
+} from '@elastic/eui'
+
+import {
+  Route,
+  Switch,
+  BrowserRouter,
+  RouteComponentProps,
+  withRouter,
+  useLocation
+} from 'react-router-dom'
+
+function NavigationTabComponent({ history }: PropsWithChildren<{}> & RouteComponentProps) {
+  const { pathname } = useLocation()
+
+  return (
+    <EuiTabs>
+      <EuiTab onClick={() => history.push('/')} isSelected={pathname === '/'}>Queue</EuiTab>
+      <EuiTab onClick={() => history.push('/submit')} isSelected={pathname === '/submit'}>New Order</EuiTab>
+    </EuiTabs>
+  )
+}
+
+const NavigationTab = withRouter(NavigationTabComponent)
 
 export function App() {
   return (
-    <h1>Hello</h1>
+    <BrowserRouter>
+      <EuiPage restrictWidth={1024}>
+        <EuiPageBody>
+          <EuiEmptyPrompt
+            iconType="database"
+            title={
+              <h1>OctoPi Print Queue</h1>
+            }
+          />
+
+          <EuiSpacer/>
+          <NavigationTab />
+
+          <Switch>
+            <Route path="/submit">
+              <h1>Submit</h1>
+            </Route>
+
+            <Route path="/">
+              <h1>Queue</h1>
+            </Route>
+          </Switch>
+        </EuiPageBody>
+      </EuiPage>
+    </BrowserRouter>
   )
 }
